b35d45d33d2f3717fe2eda909a0af09446b9bbff
TheOssumOpossum <v.bommier@gmail.com>
2016-12-14 19:45:15 -0500

    Data Scraper COmplete! MatchDetails is a class thata gathers all the information on Hirez's website other than item stats and gamemode (unfortunately not available on a single webpage) when given a matchID.

--- Program.cs
+++ Program.cs
@@ -75,80 +75,53 @@ namespace SmiteStats
         private void ReadHtml(List<string> trimmedHtml)
         {
 
-            bool winner = true;
-            //string[] substrings = { "Time:","Duration:","Kills:","Assists:","Gold:", "Kills:", "Assists:", "Gold:","placeholder" };
+            bool editingWinner = true;
+            bool skippingLines = false;
             string characterMarker = "<tr>"; //occurs twolines before the character's god (in the alt text)
-            string pictureMarker = "alt="; //identifier for gods and items
-            string numberMarker = "<td><p>"; //occurs before every statline
+            //string pictureMarker = "alt="; //identifier for gods and items
+            //string numberMarker = "<td><p>"; //occurs before every statline
+            string victoryBuildStatsMarker = "<table id=\"victory-builds-stats\"";
+            string defeatDetailsMarker = "<h4>Defeat</h4>";
+            string defeatBuildStatsMarker = "<table id=\"defeat-builds-stats\"";
 
-            //foreach (string line in trimmedHtml)
-            //if going back to the foreach loop (unlikely) replace trimmedHtml[lineNumber] with line
             for (int lineNumber = 0; lineNumber < trimmedHtml.Count; lineNumber++){
+                //ignoring build description stuff, for now atleast
+                if (trimmedHtml[lineNumber].Contains(victoryBuildStatsMarker)) {skippingLines = true;}
+                else if (trimmedHtml[lineNumber].Contains(defeatDetailsMarker)) { skippingLines = false;}
+                else if (trimmedHtml[lineNumber].Contains(defeatBuildStatsMarker)) { skippingLines = true;}
+                if (skippingLines){continue;}
+
                 if (trimmedHtml[lineNumber].Contains("Time:"))
                 {
                     ProcessTimeAndDuration(stats, trimmedHtml, lineNumber);
                 }
                 else if (trimmedHtml[lineNumber].Contains("Kills:"))
                 {
-                    if (winner)
+                    if (stats.winner.gold == 0)
                     {
                         ProcessTeamStats(stats.winner, trimmedHtml, lineNumber);
-                        winner = false;
                     }
                     else
                     {
+                        editingWinner = false;
                         ProcessTeamStats(stats.loser, trimmedHtml, lineNumber);
                     }
                 }
                 else if (trimmedHtml[lineNumber].Contains(characterMarker))
                 {
-
+                    if (editingWinner)
+                    {
+                        Player newmember = new Player();
+                        stats.winner.member.Add(newmember);
+                        ProcessTeamMember(stats.winner.member.Last(), trimmedHtml, lineNumber);
+                    }
+                    else
+                    {
+                        Player newmember = new Player();
+                        stats.loser.member.Add(newmember);
+                        ProcessTeamMember(stats.loser.member.Last(), trimmedHtml, lineNumber);
+                    }
                 }
-                //if (trimmedHtml[lineNumber].Contains(substrings[i]))
-                //{
-                //    string numberString = new string(trimmedHtml[lineNumber].Where(c => Char.IsDigit(c)).ToArray());
-                //    int numberValue = 0;
-                //    if (i != 0) { numberValue = Int32.Parse(numberString); }
-                //    switch (i)
-                //    {
-                //        case 0:
-                //            string[] dateline = trimmedHtml[lineNumber].Split(new Char[] { ':','/', ' ' ,'M' });
-                //            int monthIdx = 2;
-                //            int dayIdx = 3;
-                //            int yearIdx = 4;
-                //            int hourIdx = 5;
-                //            int minIdx = 6;
-                //            int secIdx = 7;
-                //            int pmIdx = 8;
-                //            stats.matchTime = new DateTime(Int32.Parse(dateline[yearIdx]), Int32.Parse(dateline[monthIdx]), Int32.Parse(dateline[dayIdx]),
-                //                Int32.Parse(dateline[hourIdx]) + (dateline[pmIdx] == "P" ? 12 : 0), Int32.Parse(dateline[minIdx]), Int32.Parse(dateline[secIdx]));
-                //            break;
-                //        case 1:
-                //            stats.duration = numberValue;
-                //            break;
-                //        case 2:
-                //            stats.winner.kills = numberValue;
-                //            break;
-                //        case 3:
-                //            stats.winner.assists = numberValue;
-                //            break;
-                //        case 4:
-                //            stats.winner.gold = numberValue;
-                //            break;
-                //        case 5:
-                //            stats.loser.kills = numberValue;
-                //            break;
-                //        case 6:
-                //            stats.loser.assists = numberValue;
-                //            break;
-                //        case 7:
-                //            stats.loser.gold = numberValue;
-                //            break;
-                //        default:
-                //            break;
-                //    }
-                //    i++;               
-                //}
             }
         }
 
@@ -156,6 +129,28 @@ namespace SmiteStats
         {
             string numbers = new string(line.Where(c => Char.IsDigit(c)).ToArray());
             return Int32.Parse(numbers);
+        }
+
+        private static string GetPictureName(string line)
+        {
+            string[] pictureLine = line.Split(new string[] { "alt=\"", "\" />", "\"/>" }, StringSplitOptions.None);
+            return pictureLine[1];
+        }
+
+        private static void GetPlayerName(Player player, string line)
+        {
+            string[] playerLine = line.Split(new string[] { "[", "]", "<p>", "</p>" }, StringSplitOptions.None);
+            int clanLength = 5;
+            if (playerLine.Length == clanLength)
+            {
+                player.name = playerLine[3];
+                player.clan = playerLine[2];
+            }
+            else
+            {
+                player.name = playerLine[2];
+                player.clan = null;
+            }
         }
 
         private void ProcessTeamStats(Team team, List<string> html, int lineNumber)
@@ -170,7 +165,7 @@ namespace SmiteStats
 
         private void ProcessTimeAndDuration(MatchStats match, List<string> html, int lineNumber)
         {
-            int DurationSeparation = 3;
+            int durationSeparation = 3;
             int monthIdx = 2;
             int dayIdx = 3;
             int yearIdx = 4;
@@ -178,10 +173,74 @@ namespace SmiteStats
             int minIdx = 6;
             int secIdx = 7;
             int pmIdx = 8;
-            string[] dateline = html[lineNumber].Split(new Char[] { ':', '/', ' ', 'M' });
-            match.matchTime = new DateTime(Int32.Parse(dateline[yearIdx]), Int32.Parse(dateline[monthIdx]), Int32.Parse(dateline[dayIdx]),
-                Int32.Parse(dateline[hourIdx]) + (dateline[pmIdx] == "P" ? 12 : 0), Int32.Parse(dateline[minIdx]), Int32.Parse(dateline[secIdx]));
-            match.duration = GetNumbersFromLine(html[lineNumber + DurationSeparation]);
+            string[] dateLine = html[lineNumber].Split(new Char[] { ':', '/', ' ', 'M' });
+            match.matchTime = new DateTime(Int32.Parse(dateLine[yearIdx]), Int32.Parse(dateLine[monthIdx]), Int32.Parse(dateLine[dayIdx]),
+                Int32.Parse(dateLine[hourIdx]) + (dateLine[pmIdx] == "P" ? 12 : 0), Int32.Parse(dateLine[minIdx]), Int32.Parse(dateLine[secIdx]));
+            match.duration = GetNumbersFromLine(html[lineNumber + durationSeparation]);
+        }
+
+        private void ProcessTeamMember(Player member,List<string> html, int lineNumber)
+        {
+            int godOffset = 2;
+            int nameOffset = 4;
+            int levelOffset = 6;
+            int kdaOffset = 7;
+            int spreeOffset = 8;
+            int multiOffset = 9;
+            int goldOffset = 10;
+            int gpmOffset = 11;
+            int damageOffset = 12;
+            int towerOffset = 13;
+            int healOffset = 14;
+            int minionOffset = 15;
+            int takenOffset = 16;
+            member.god = GetPictureName(html[lineNumber + godOffset]);
+            GetPlayerName(member, html[lineNumber + nameOffset]);
+            member.level = GetNumbersFromLine(html[lineNumber + levelOffset]);
+            ProcessKDA(member, html[lineNumber + kdaOffset]);
+            member.bestKillSpree = GetNumbersFromLine(html[lineNumber + spreeOffset]);
+            member.bestMultiKill = GetNumbersFromLine(html[lineNumber + multiOffset]);
+            member.gold = GetNumbersFromLine(html[lineNumber + goldOffset]);
+            member.goldPerMinute = GetNumbersFromLine(html[lineNumber + gpmOffset]);
+            member.playerDamage = GetNumbersFromLine(html[lineNumber + damageOffset]);
+            member.towerDamage = GetNumbersFromLine(html[lineNumber + towerOffset]);
+            member.healing = GetNumbersFromLine(html[lineNumber + healOffset]);
+            member.minionDamage = GetNumbersFromLine(html[lineNumber + minionOffset]);
+            member.damageTaken = GetNumbersFromLine(html[lineNumber + takenOffset]);
+
+            lineNumber = lineNumber + 17;
+            //string
+            //while (!line.Contains("</tr>"))
+            //{
+
+            //    lineNumber++;
+            //    line = html[lineNumber];
+            //}
+            int itemNumber = 0;
+            for (string line = html[lineNumber]; !line.Contains("</tr>");line = html[lineNumber++])
+            {
+                if (line.Contains("alt="))
+                {
+                    if (itemNumber < 2)
+                    {
+                        member.relic[itemNumber] = new Item(GetPictureName(line));
+                        itemNumber++;
+                    }
+                    else
+                    {
+                        member.build[itemNumber - 2] = new Item(GetPictureName(line));
+                        itemNumber++;
+                    }
+                }
+            }
+        }
+
+        private void ProcessKDA(Player player,string line)
+        {
+            string[] kdaString = line.Split(new string[] { "<td><p>", "/", "</p></td>"},StringSplitOptions.None);
+            player.kills = Int32.Parse(kdaString[1]);
+            player.deaths = Int32.Parse(kdaString[2]);
+            player.assists = Int32.Parse(kdaString[3]);
         }
 
         class MatchStats
@@ -200,14 +259,14 @@ namespace SmiteStats
 
         class Team
         {
-            public Player[] member;
+            public List<Player> member;
             public int kills;
             public int assists;
             public int gold;
 
             public Team()
             {
-
+                this.member = new List<Player>();
             }
 
 
@@ -217,13 +276,14 @@ namespace SmiteStats
         {
             public string name, clan;
             public string god;
-            public int level, kills, deaths, assists, gold, bestKillSpree, bestMultiKill, playerDamage, towerDamage, healing, minionDamage, damageTaken;
-            public Item[] relic = new Item[2];
-            public Item[] build = new Item[6];
+            public int level, kills, deaths, assists, gold, goldPerMinute, bestKillSpree, bestMultiKill, playerDamage, towerDamage, healing, minionDamage, damageTaken;
+            public Item[] relic;
+            public Item[] build;
 
             public Player()
             {
-
+                relic = new Item[2];
+                build = new Item[6];
             }
 
 
